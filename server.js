76d59445988cb3bf3224f4a165b1de088da56ed8
adatapost <adatapost@gmail.com>
2013-08-09 12:15:04 +0530

    update server.js

--- server.js
+++ server.js
@@ -2,5 +2,6 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<h1>A DATAPOST</h1>');
+  res.end('<h1>A DATAPOST COMPUTER CENTRE</h1>');
+  res.end('<h1>MEHSANA</h1>');
 }).listen(port);
